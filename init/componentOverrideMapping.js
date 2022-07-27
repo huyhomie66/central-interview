1bd9e6e53c1182dc24bbbb8c1ce0c217120d8722
liam <liam@liams-MacBook-Pro.local>
2022-07-27 17:34:39 +0700

    scroll thumnail

--- init/componentOverrideMapping.js
+++ init/componentOverrideMapping.js
@@ -1,4 +1,6 @@
 module.exports = componentOverride = {
     [`@magento/venia-ui/lib/components/Gallery/item.js`]: 'src/components/Gallery/item.js',
-    [`@magento/venia-ui/lib/components/Gallery/item.shimmer.js`]: 'src/components/Gallery/item.shimmer.js'
+    [`@magento/venia-ui/lib/components/Gallery/item.shimmer.js`]: 'src/components/Gallery/item.shimmer.js',
+    [`@magento/venia-ui/lib/components/ProductImageCarousel/carousel.js`]: 'src/components/ProductImageCarousel/carousel.js',
+    [`@magento/venia-ui/lib/components/ProductImageCarousel/thumbnail.js`]: 'src/components/ProductImageCarousel/thumbnail.js'
 };
